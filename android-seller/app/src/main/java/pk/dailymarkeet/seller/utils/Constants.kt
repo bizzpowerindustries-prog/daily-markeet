04e88cfdab3e2180bf0268e7b503e381725fc2dc
bizzpowerindustries-prog <bizzpowerindustries@gmail.com>
2026-07-12 13:23:31 -0700

    Create Constants.kt

---
+++ android-seller/app/src/main/java/pk/dailymarkeet/seller/utils/Constants.kt
@@ -0,0 +1,8 @@
+package pk.dailymarkeet.seller.utils
+
+object Constants {
+    const val BASE_URL = "https://dailymarkeet.pk/"
+    const val PREF_NAME = "DailyMarkeetSellerPref"
+    const val TOKEN_KEY = "seller_auth_token"
+    const val USER_KEY = "seller_user_data"
+}
